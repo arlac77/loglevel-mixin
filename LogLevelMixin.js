6522aeb5362473000c1908f28385b4f17c4d606c
Torsten Link <torsten.link@db.com>
2015-12-28 13:43:49 +0100

    style(doc): fix some typos  and add some inline documentation

--- LogLevelMixin.js
+++ LogLevelMixin.js
@@ -7,6 +7,12 @@ const logLevels = declareLevels(['trace', 'debug', 'info', 'notice', 'warn', 'er
 
 exports.defaultLogLevels = logLevels;
 
+
+/**
+ * Generate the loglevel objects out of a list of log level names.
+ * @param {Array} list A list of log level names. The last name in the list will become the one with the highest priority.
+ * @return {Object} levels object A hash with all the loglevels. Stored by there name.
+ */
 function declareLevels(list) {
   const levels = {};
   let priority = list.length;
@@ -24,10 +30,10 @@ function declareLevels(list) {
 
 /**
  * Adds logging methods to an existing object.
- * For each loglevel a methos will be created.
- * info()
+ * For each loglevel a method with the name of the log level will be created.
  * @param {Object} target object where to assign properties tp
- * @param {Function} theFunction Optional function to be added under the loglevel name.
+ * @param {Object} logLevels Hash with all the available loglevels. Stored by there name
+ * @param {Function, optional} theFunction The function to be added under the loglevel name.
  *        This function will only be called if the current loglevel is greater equal
  *        the log level of the called logging function.
  *        By default a method log(leve,message) will be used
@@ -59,10 +65,12 @@ exports.defineLoggerMethods = function (object, logLevels, theFunction) {
  * Declares two properties:
  *  logLevel {String} 'info','error',...
  *  logLevelPriority {Number}
+ * This is method if for classes
  * @param {Object} properties target object where the properties will be written into
+ * @param {Object} logLevels Hash with all the available loglevels. Stored by there name
  * @param {String} defaultLogLevel the default value for the properties
  */
-module.exports.LogLevelMixin = (superclass, logLevels, defaultLogLevel) => class extends superclass {
+exports.LogLevelMixin = (superclass, logLevels, defaultLogLevel) => class extends superclass {
   constructor() {
     super();
     this._logLevel = defaultLogLevel;
@@ -84,7 +92,9 @@ module.exports.LogLevelMixin = (superclass, logLevels, defaultLogLevel) => class
  * Declares two properties:
  *  logLevel {String} 'info','error',...
  *  logLevelPriority {Number}
+ * 
  * @param {Object} properties target object where the properties will be written into
+ * @param {Object} logLevels Hash with all the available loglevels. Stored by there name
  * @param {String} defaultLogLevel the default value for the properties
  */
 exports.defineLogLevelProperties = function (object, logLevels, defaultLogLevel) {
