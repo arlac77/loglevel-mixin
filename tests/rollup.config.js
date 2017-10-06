bcd07759016f3eb1e9639af2816c008dcda48cc7
Markus Felten <Markus.Felten@gmx.de>
2017-10-06 18:48:14 +0200

    chore(rollup): do not use map

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,11 +1,33 @@
-export default ['base', 'es6-class', 'supporting-functions'].map(name => {
-  return {
-    input: `tests/${name}-test.js`,
+const external = ['ava'];
+const format = 'cjs';
+const sourcemap = true;
+
+export default [
+  {
+    input: `tests/base-test.js`,
     output: {
-      file: `build/${name}-test.js`,
-      format: 'cjs',
-      sourcemap: true
+      file: `build/base-test.js`,
+      format,
+      sourcemap
     },
-    external: ['ava']
-  };
-});
+    external
+  },
+  {
+    input: `tests/es6-class-test.js`,
+    output: {
+      file: `build/es6-class-test.js`,
+      format,
+      sourcemap
+    },
+    external
+  },
+  {
+    input: `tests/supporting-functions-test.js`,
+    output: {
+      file: `build/supporting-functions-test.js`,
+      format,
+      sourcemap
+    },
+    external
+  }
+];
